d1b121cf63bcd2fda5f190a6ec3bd4bb36b2d7c4
AgustinGomezDelToro <66341782+AgustinGomezDelToro@users.noreply.github.com>
2023-07-21 11:39:39 +0200

    index updated

--- web/app/src/pages/index.tsx
+++ web/app/src/pages/index.tsx
@@ -1,6 +1,5 @@
-
 import React, { useEffect, useState } from "react";
-import { Menu } from '../Layout';
+import Head from "next/head";
 
 const Index = () => {
     const [zoo, setZoo] = useState(null);
@@ -21,10 +20,18 @@ const Index = () => {
 
     return (
         <div>
-                <div>
-                    <h1>{zoo.name}</h1>
-                    <p>{zoo.description}</p>
-                </div>
+            <Head>
+                <link rel="stylesheet" href="/css/Index.css" />
+            </Head>
+            <h1>Bienvenue au ZOO de l'ESGI</h1>
+            <p className="welcome-text">
+                Venez découvrir et apprendre sur les merveilleuses créatures qui habitent notre planète!
+                Ici à ZOO de l'ESGI, nous vous offrons une expérience mémorable qui rapproche vous et la nature.
+                Avec une variété de différentes attractions, y compris la majestueuse lion de la savane africaine,
+                les colorés perroquets de la jungle amazonienne, et plus encore, il y a quelque chose pour tout le monde
+                au ZOO de l'ESGI.
+            </p>
+            <a href="/login" className="login-button">Se connecter</a>
         </div>
     );
 };
